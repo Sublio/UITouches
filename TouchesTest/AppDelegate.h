364d0f91b2f60beb099b0acea19654207745f407
Sublio <sublio1260@gmail.com>
2017-06-09 22:33:11 +0600

    Initial Commit

---
+++ TouchesTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchesTest
+//
+//  Created by Denis on 09.06.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchesTest
+//
+//  Created by Denis on 09.06.17.
+//  Copyright © 2017 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
